6c81da47bbb3b164252b489bad1be57fb4a83396
Hanani <hananee92@gmail.com>
2020-06-10 01:32:48 +0800

    Get single track by id

--- lib/services/tracks_service.dart
+++ lib/services/tracks_service.dart
@@ -1,12 +1,19 @@
 import 'package:tracks/services/networking.dart';
 
-const apiURL = 'https://api.popsical.tv/v3/songs.json';
+const apiURL = 'https://api.popsical.tv/v3/songs';
 
-class TracksModel {
+class TracksService {
   Future<dynamic> getTrackList() async {
-    NetworkHelper networkHelper = NetworkHelper(apiURL);
+    NetworkHelper networkHelper = NetworkHelper('$apiURL.json');
 
     var tracksData = await networkHelper.getData();
     return tracksData;
+  }
+
+  Future<dynamic> getTrack(int id) async {
+    NetworkHelper networkHelper = NetworkHelper('$apiURL/$id.json');
+
+    var trackData = await networkHelper.getData();
+    return trackData;
   }
 }
