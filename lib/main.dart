555778c86a57faa598bb4fad7cd9f2fe93a68575
Hanani <hananee92@gmail.com>
2020-06-10 09:46:11 +0800

    Set theme

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.blue,
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Colors.pinkAccent,
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
       ),
       home: TracksScreen(),
     );
